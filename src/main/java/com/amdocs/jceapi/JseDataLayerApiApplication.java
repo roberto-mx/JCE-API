0c380b1b9a2208e96c836aae5b30b9ac4524fdf8
Gustavo <kyg7887@gmail.com>
2020-08-06 01:44:25 -0500

    addin schema and db properties

--- src/main/java/com/amdocs/jceapi/JseDataLayerApiApplication.java
+++ src/main/java/com/amdocs/jceapi/JseDataLayerApiApplication.java
@@ -3,6 +3,7 @@ package com.amdocs.jceapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableJpaRepositories
 public class JseDataLayerApiApplication {
 
 	public static void main(String[] args) {
